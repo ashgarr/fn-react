d099838d7007a357a238836de89fcd3cca9ca028
jingleHeatmap <853625441@qq.com>
2019-02-24 18:13:17 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      numClicks: 0
+    }
+
+  }
+  handleClick = () => {
+    this.setState({
+      numClicks: this.state.numClicks + 1
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            pressed: {this.state.numClicks}
           </p>
           <a
             className="App-link"
@@ -19,6 +34,14 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button 
+            variant="contained"
+            color="primary"
+            onClick={this.handleClick}
+            >
+            Button
+          </Button>
+
         </header>
       </div>
     );
