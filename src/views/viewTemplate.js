f8b5eb2364b56cff46c3ff7e45c71d0595011e04
connor <mcwvproductions@gmail.com>
2019-02-26 10:30:45 -0800

    add template!

---
+++ src/views/viewTemplate.js
@@ -0,0 +1,16 @@
+// baseline code for views and components
+
+import React, { Component } from 'react'
+import '../App.css'
+
+class NAME extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default NAME
