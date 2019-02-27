28f36f65b063f511ab01929f2c9d3ccb3c2d9dbf
jingleHeatmap <853625441@qq.com>
2019-02-27 15:54:32 -0800

    jingle test verify

--- src/views/Verify.js
+++ src/views/Verify.js
@@ -5,7 +5,7 @@ class Verify extends Component {
     render() {
         return(
             <div>
-                <p>contentless verify page</p>
+                <p>jingle-test ~~~~ contentless verify page</p>
             </div>
         )
     }
