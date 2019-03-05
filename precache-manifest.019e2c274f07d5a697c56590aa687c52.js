3f7a0f495216cfdd415891897e1bf492ae43a4d0
jingle <853625441@qq.com>
2019-03-04 16:17:22 -0800

    Updates

---
+++ precache-manifest.019e2c274f07d5a697c56590aa687c52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9636b3eb54691d866218",
+    "url": "/fn-react/static/js/runtime~main.9636b3eb.js"
+  },
+  {
+    "revision": "e007736c057d3d6562b9",
+    "url": "/fn-react/static/js/main.e007736c.chunk.js"
+  },
+  {
+    "revision": "7211c6b701f3d438822e",
+    "url": "/fn-react/static/js/2.7211c6b7.chunk.js"
+  },
+  {
+    "revision": "e007736c057d3d6562b9",
+    "url": "/fn-react/static/css/main.a22e082f.chunk.css"
+  },
+  {
+    "revision": "171813c69af8b598f35d6dce2082dd59",
+    "url": "/fn-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fn-react/precache-manifest.35c708632f06c8c5153f3ff9e5c4e7b7.js"
+  "/fn-react/precache-manifest.019e2c274f07d5a697c56590aa687c52.js"
 );
 
 workbox.clientsClaim();
