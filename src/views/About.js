4331f81a668333562d8fb71c790fa1fb896bf6c2
jingle <853625441@qq.com>
2019-03-04 17:44:07 -0800

    Jingle: some about us page

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 import '../App.css'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 class About extends Component {
     render() {
-        return(
+        return (
             <div>
-                <p>contentless about page</p>
+
+                <Jumbotron>
+                    <blockquote class="blockquote mb-0">
+                        <p class="font-size">When we hear news, we should always wait for the sacrament of confirmation.</p>
+                        <footer class="blockquote-footer">Voltaire in <cite title="Source Title">Letter to Charles-Augustin Ferriol, August 28, 1760</cite></footer>
+                    </blockquote>
+                </Jumbotron>
+
+                <div class="ml-5 text-left">
+                    <br />
+                    <h5>
+                        What Are Fake News?
+                    </h5>
+                    <div class="card">
+
+                    </div>
+                    <p>
+                        While there are various definitions of fake news,
+                            </p>
+                </div>
+
+
+
+
             </div>
         )
     }
